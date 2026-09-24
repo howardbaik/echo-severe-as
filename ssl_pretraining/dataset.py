c2cecf8aead08d5faeee1402e7a0e81d00080c5b
howardbaik <howardbaik43@gmail.com>
2026-09-24 12:16:11 -0400

    Reformat ssl_pretraining/dataset.py

--- ssl_pretraining/dataset.py
+++ ssl_pretraining/dataset.py
@@ -12,6 +12,7 @@ from scipy.ndimage import rotate
 
 from utils import load_video
 
+
 class EchoDataset(torch.utils.data.Dataset):
     """
     PyTorch dataset that loads echocardiogram videos for self-supervised pretraining. This extends the SimCLR framework by selecting two *different* videos from the
@@ -38,7 +39,7 @@ class EchoDataset(torch.utils.data.Dataset):
         List of paths to "video #2" to be returned at each iteration
     temporal_orderings : list[tuple]
         List of all permutations of frame indices (target classes for frame re-ordering task)
-    
+
     Methods
     -------
     _sample_frames(x)
@@ -50,23 +51,26 @@ class EchoDataset(torch.utils.data.Dataset):
     __getitem__
         Returns video clip #1, video clip #2, frame order label #1, and frame order label #2 as described above
     """
+
     def __init__(self, data_dir, split, clip_len=16, sampling_rate=1, n=None):
         self.split = split
         self.clip_len = clip_len
         self.sampling_rate = sampling_rate
 
-        self.video_dir = os.path.join(data_dir, 'videos')
-        self.label_df = pd.read_csv(os.path.join(data_dir, self.split + '.csv'))
+        self.video_dir = os.path.join(data_dir, "videos")
+        self.label_df = pd.read_csv(os.path.join(data_dir, self.split + ".csv"))
 
         if n is not None:
             self.label_df = self.label_df.iloc[:n, :]
 
-        study_ids = np.sort(self.label_df['acc_num'].unique())
+        study_ids = np.sort(self.label_df["acc_num"].unique())
 
         self.fnames_i = []
         self.fnames_j = []
         for study_id in tqdm.tqdm(study_ids):
-            fnames = self.label_df[self.label_df['acc_num'] == study_id]['fpath'].values.tolist()
+            fnames = self.label_df[self.label_df["acc_num"] == study_id][
+                "fpath"
+            ].values.tolist()
 
             if len(fnames) == 1:
                 self.fnames_i.append(fnames[0])
@@ -76,15 +80,27 @@ class EchoDataset(torch.utils.data.Dataset):
                     self.fnames_i.append(fname_pair[0])
                     self.fnames_j.append(fname_pair[1])
 
-        self.temporal_orderings = [_ for _ in itertools.permutations(np.arange(self.clip_len))]
+        self.temporal_orderings = [
+            _ for _ in itertools.permutations(np.arange(self.clip_len))
+        ]
 
     def _sample_frames(self, x):
-        if x.shape[0] > self.clip_len*self.sampling_rate:
-            start_idx = np.random.choice(x.shape[0]-self.clip_len*self.sampling_rate, size=1)[0]
-            x = x[start_idx:(start_idx+self.clip_len*self.sampling_rate):self.sampling_rate]
+        if x.shape[0] > self.clip_len * self.sampling_rate:
+            start_idx = np.random.choice(
+                x.shape[0] - self.clip_len * self.sampling_rate, size=1
+            )[0]
+            x = x[
+                start_idx : (
+                    start_idx + self.clip_len * self.sampling_rate
+                ) : self.sampling_rate
+            ]
         else:
-            x = x[::self.sampling_rate]
-            x = np.pad(x, ((0, self.clip_len-x.shape[0]), (0, 0), (0, 0), (0, 0)), mode='constant')
+            x = x[:: self.sampling_rate]
+            x = np.pad(
+                x,
+                ((0, self.clip_len - x.shape[0]), (0, 0), (0, 0), (0, 0)),
+                mode="constant",
+            )
 
         return x
 
@@ -96,7 +112,7 @@ class EchoDataset(torch.utils.data.Dataset):
         temp = np.zeros((l, h + 2 * pad, w + 2 * pad, c), dtype=x.dtype)
         temp[:, pad:-pad, pad:-pad, :] = x
         i, j = np.random.randint(0, 2 * pad, 2)
-        x = temp[:, i:(i + h), j:(j + w), :]
+        x = temp[:, i : (i + h), j : (j + w), :]
 
         # Random horizontal flip
         if random.uniform(0, 1) > 0.5:
@@ -118,7 +134,7 @@ class EchoDataset(torch.utils.data.Dataset):
     def __len__(self):
         return len(self.fnames_i)
 
-    def __getitem__(self, idx): 
+    def __getitem__(self, idx):
         x_i = load_video(os.path.join(self.video_dir, self.fnames_i[idx]))
         x_j = load_video(os.path.join(self.video_dir, self.fnames_j[idx]))
 
@@ -139,7 +155,13 @@ class EchoDataset(torch.utils.data.Dataset):
         x_i = np.transpose(x_i, (3, 0, 1, 2))
         x_j = np.transpose(x_j, (3, 0, 1, 2))
 
-        return torch.from_numpy(x_i).float(), torch.from_numpy(x_j).float(), torch.from_numpy(reordering_i).long(), torch.from_numpy(reordering_j).long()
+        return (
+            torch.from_numpy(x_i).float(),
+            torch.from_numpy(x_j).float(),
+            torch.from_numpy(reordering_i).long(),
+            torch.from_numpy(reordering_j).long(),
+        )
+
 
 class EchoNetLVHDataset(EchoDataset):
     """
@@ -164,33 +186,65 @@ class EchoNetLVHDataset(EchoDataset):
     temporal_orderings : list[tuple]
         List of all permutations of frame indices (target classes for frame re-ordering task)
     """
-    def __init__(self, data_dir, split, clip_len=4, sampling_rate=1, frame_size=112, same_clip=False, n=None):
+
+    def __init__(
+        self,
+        data_dir,
+        split="train",
+        clip_len=4,
+        sampling_rate=1,
+        frame_size=112,
+        same_clip=False,
+        n=None,
+    ):
         self.split = split
         self.clip_len = clip_len
         self.sampling_rate = sampling_rate
         self.frame_size = frame_size
         self.same_clip = same_clip
 
-        label_df = pd.read_csv(os.path.join(data_dir, 'MeasurementsList.csv'))
-        names = np.sort(label_df.loc[label_df['split'] == self.split, 'HashedFileName'].unique())
+        label_df = pd.read_csv(os.path.join(data_dir, "MeasurementsList.csv"))
+        names = np.sort(
+            label_df.loc[label_df["split"] == self.split, "HashedFileName"].unique()
+        )
 
-        batch_dirs = sorted(d for d in os.listdir(data_dir) if d.startswith('Batch') and os.path.isdir(os.path.join(data_dir, d)))
+        batch_dirs = sorted(
+            d
+            for d in os.listdir(data_dir)
+            if d.startswith("Batch") and os.path.isdir(os.path.join(data_dir, d))
+        )
         available = {}
         for batch_dir in batch_dirs:
             for fname in os.listdir(os.path.join(data_dir, batch_dir)):
-                if fname.endswith('.avi'):
-                    available.setdefault(fname[:-4], os.path.join(data_dir, batch_dir, fname))
+                if fname.endswith(".avi"):
+                    available.setdefault(
+                        fname[:-4], os.path.join(data_dir, batch_dir, fname)
+                    )
 
         self.fnames = [available[name] for name in names if name in available]
-        print(f'{self.split}: found {len(self.fnames)} of {len(names)} videos ({len(names) - len(self.fnames)} missing on disk)')
+        print(
+            f"{self.split}: found {len(self.fnames)} of {len(names)} videos ({len(names) - len(self.fnames)} missing on disk)"
+        )
 
         if n is not None:
             self.fnames = self.fnames[:n]
 
-        self.temporal_orderings = [_ for _ in itertools.permutations(np.arange(self.clip_len))]
+        self.temporal_orderings = [
+            _ for _ in itertools.permutations(np.arange(self.clip_len))
+        ]
 
     def _resize(self, x):
-        return np.stack([cv2.resize(frame, (self.frame_size, self.frame_size), interpolation=cv2.INTER_AREA) for frame in x], axis=0)
+        return np.stack(
+            [
+                cv2.resize(
+                    frame,
+                    (self.frame_size, self.frame_size),
+                    interpolation=cv2.INTER_AREA,
+                )
+                for frame in x
+            ],
+            axis=0,
+        )
 
     @staticmethod
     def _normalize(x):
@@ -215,4 +269,9 @@ class EchoNetLVHDataset(EchoDataset):
         x_i = np.transpose(self._normalize(x_i), (3, 0, 1, 2))
         x_j = np.transpose(self._normalize(x_j), (3, 0, 1, 2))
 
-        return torch.from_numpy(x_i).float(), torch.from_numpy(x_j).float(), torch.tensor(reordering_i).long(), torch.tensor(reordering_j).long()
+        return (
+            torch.from_numpy(x_i).float(),
+            torch.from_numpy(x_j).float(),
+            torch.tensor(reordering_i).long(),
+            torch.tensor(reordering_j).long(),
+        )
